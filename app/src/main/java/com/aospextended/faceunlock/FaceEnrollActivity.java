xdroid-oss/xd_packages_apps_FaceUnlockService

Cleanup vendor impl
Change-Id: I854bdf580287b2033db3803f299cc55ac63eb3d6

app/src/main/java/org/pixelexperience/faceunlock/FaceEnrollActivity.java
@@ -1,5 +1,20 @@
 package org.pixelexperience.faceunlock;
 
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_DARKLIGHT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_BLUR;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_MULTI;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_NOT_COMPLETE;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_OFFSET_LEFT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_OFFSET_RIGHT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_ROTATED_LEFT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_ROTATED_RIGHT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_LARGE;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_SMALL;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FAILED;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_HALF_SHADOW;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_HIGHLIGHT;
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_KEEP;
+
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -22,21 +37,6 @@ import org.pixelexperience.faceunlock.util.CircleSurfaceView;
 import org.pixelexperience.faceunlock.util.Settings;
 import org.pixelexperience.faceunlock.util.SharedUtil;
 import org.pixelexperience.faceunlock.util.Util;
-
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_DARKLIGHT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_BLUR;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_MULTI;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_NOT_COMPLETE;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_OFFSET_LEFT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_OFFSET_RIGHT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_ROTATED_LEFT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_ROTATED_RIGHT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_LARGE;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_SMALL;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FAILED;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_HALF_SHADOW;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_HIGHLIGHT;
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_KEEP;
 
 public class FaceEnrollActivity extends FaceBaseActivity {
     private static final String TAG = FaceEnrollActivity.class.getSimpleName();

app/src/main/java/org/pixelexperience/faceunlock/camera/CameraFaceAuthController.java
@@ -1,5 +1,7 @@
 package org.pixelexperience.faceunlock.camera;
 
+import static org.pixelexperience.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_NOT_FOUND;
+
 import android.content.Context;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
@@ -19,8 +21,6 @@ import org.pixelexperience.faceunlock.camera.listeners.ReadParametersListener;
 import org.pixelexperience.faceunlock.util.Util;
 
 import java.nio.ByteBuffer;
-
-import static org.pixelexperience.faceunlock.FaceConstants.MG_UNLOCK_FACE_NOT_FOUND;
 
 public class CameraFaceAuthController {
     private static final int CAM_MSG_ERROR = 101;

app/src/main/java/org/pixelexperience/faceunlock/service/FaceAuthService.java
@@ -1,6 +1,8 @@
 package org.pixelexperience.faceunlock.service;
 
-import static android.hardware.biometrics.BiometricConstants.*;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_LOCKOUT;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_LOCKOUT_PERMANENT;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_VENDOR;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -24,11 +26,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
-import com.android.internal.util.custom.faceunlock.FaceUnlockUtils;
 import com.android.internal.util.custom.faceunlock.IFaceService;
 import com.android.internal.util.custom.faceunlock.IFaceServiceReceiver;
 
 import org.pixelexperience.faceunlock.AppConstants;
+import org.pixelexperience.faceunlock.R;
 import org.pixelexperience.faceunlock.camera.CameraFaceAuthController;
 import org.pixelexperience.faceunlock.camera.CameraFaceEnrollController;
 import org.pixelexperience.faceunlock.camera.CameraUtil;
@@ -36,7 +38,8 @@ import org.pixelexperience.faceunlock.util.NotificationUtils;
 import org.pixelexperience.faceunlock.util.Settings;
 import org.pixelexperience.faceunlock.util.SharedUtil;
 import org.pixelexperience.faceunlock.util.Util;
-import org.pixelexperience.faceunlock.vendor.FacePPImpl;
+import org.pixelexperience.faceunlock.vendor.VendorFaceManager;
+import org.pixelexperience.faceunlock.vendor.impl.VendorFaceManagerImpl;
 
 import java.util.Random;
 
@@ -58,7 +61,7 @@ public class FaceAuthService extends Service {
     private long mChallenge = 0;
     private int mChallengeCount = 0;
     private byte[] mEnrollToken;
-    private FacePPImpl mFaceAuth;
+    private VendorFaceManager mFaceAuth;
     private PendingIntent mIdleTimeoutIntent;
     private boolean mOnIdleTimer;
     private Integer mLockoutType = LOCKOUT_TYPE_DISABLED;
@@ -284,7 +287,7 @@ public class FaceAuthService extends Service {
         handlerThread.start();
         mWorkHandler = new FaceHandler(handlerThread.getLooper());
         mShareUtil = new SharedUtil(this);
-        mFaceAuth = new FacePPImpl(this);
+        mFaceAuth = new VendorFaceManagerImpl(this, getResources().getBoolean(R.bool.use_alternative_vendor_impl));
         mUserId = Util.getUserId(this);
         if (!Util.isFaceUnlockDisabledByDPM(this) && Util.isFaceUnlockEnrolled(this)) {
             mWorkHandler.post(() -> mFaceAuth.init());

aex-tmp/packages_apps_FaceUnlockService

Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
Signed-off-by: kunalshah <kunalshah912@gmail.com>

app/src/main/java/com/aospextended/faceunlock/FaceEnrollActivity.java
@@ -1,5 +1,20 @@
 package com.aospextended.faceunlock;
 
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_DARKLIGHT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_BLUR;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_MULTI;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_NOT_COMPLETE;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_OFFSET_LEFT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_OFFSET_RIGHT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_ROTATED_LEFT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_ROTATED_RIGHT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_LARGE;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_SMALL;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FAILED;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_HALF_SHADOW;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_HIGHLIGHT;
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_KEEP;
+
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -22,21 +37,6 @@ import com.aospextended.faceunlock.util.CircleSurfaceView;
 import com.aospextended.faceunlock.util.Settings;
 import com.aospextended.faceunlock.util.SharedUtil;
 import com.aospextended.faceunlock.util.Util;
-
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_DARKLIGHT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_BLUR;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_MULTI;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_NOT_COMPLETE;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_OFFSET_LEFT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_OFFSET_RIGHT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_ROTATED_LEFT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_ROTATED_RIGHT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_LARGE;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_SCALE_TOO_SMALL;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FAILED;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_HALF_SHADOW;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_HIGHLIGHT;
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_KEEP;
 
 public class FaceEnrollActivity extends FaceBaseActivity {
     private static final String TAG = FaceEnrollActivity.class.getSimpleName();

app/src/main/java/com/aospextended/faceunlock/camera/CameraFaceAuthController.java
@@ -1,5 +1,7 @@
 package com.aospextended.faceunlock.camera;
 
+import static com.aospextended.faceunlock.vendor.constants.FaceConstants.MG_UNLOCK_FACE_NOT_FOUND;
+
 import android.content.Context;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
@@ -19,8 +21,6 @@ import com.aospextended.faceunlock.camera.listeners.ReadParametersListener;
 import com.aospextended.faceunlock.util.Util;
 
 import java.nio.ByteBuffer;
-
-import static com.aospextended.faceunlock.FaceConstants.MG_UNLOCK_FACE_NOT_FOUND;
 
 public class CameraFaceAuthController {
     private static final int CAM_MSG_ERROR = 101;

app/src/main/java/com/aospextended/faceunlock/service/FaceAuthService.java
@@ -1,6 +1,8 @@
 package com.aospextended.faceunlock.service;
 
-import static android.hardware.biometrics.BiometricConstants.*;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_LOCKOUT;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_LOCKOUT_PERMANENT;
+import static android.hardware.biometrics.BiometricConstants.BIOMETRIC_ERROR_VENDOR;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -24,19 +26,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
-import com.android.internal.util.custom.faceunlock.FaceUnlockUtils;
 import com.android.internal.util.custom.faceunlock.IFaceService;
 import com.android.internal.util.custom.faceunlock.IFaceServiceReceiver;
 
 import com.aospextended.faceunlock.AppConstants;
+import com.aospextended.faceunlock.R;
 import com.aospextended.faceunlock.camera.CameraFaceAuthController;
 import com.aospextended.faceunlock.camera.CameraFaceEnrollController;
 import com.aospextended.faceunlock.camera.CameraUtil;
 import com.aospextended.faceunlock.util.NotificationUtils;
 import com.aospextended.faceunlock.util.SharedUtil;
 import com.aospextended.faceunlock.util.Util;
-import com.aospextended.faceunlock.vendor.FacePPImpl;
 import com.aospextended.faceunlock.util.Settings;
+import com.aospextended.faceunlock.vendor.VendorFaceManager;
+import com.aospextended.faceunlock.vendor.impl.VendorFaceManagerImpl;
 
 import java.util.Random;
 
@@ -58,7 +61,7 @@ public class FaceAuthService extends Service {
     private long mChallenge = 0;
     private int mChallengeCount = 0;
     private byte[] mEnrollToken;
-    private FacePPImpl mFaceAuth;
+    private VendorFaceManager mFaceAuth;
     private PendingIntent mIdleTimeoutIntent;
     private boolean mOnIdleTimer;
     private Integer mLockoutType = LOCKOUT_TYPE_DISABLED;
@@ -284,7 +287,7 @@ public class FaceAuthService extends Service {
         handlerThread.start();
         mWorkHandler = new FaceHandler(handlerThread.getLooper());
         mShareUtil = new SharedUtil(this);
-        mFaceAuth = new FacePPImpl(this);
+        mFaceAuth = new VendorFaceManagerImpl(this, getResources().getBoolean(R.bool.use_alternative_vendor_impl));
         mUserId = Util.getUserId(this);
         if (!Util.isFaceUnlockDisabledByDPM(this) && Util.isFaceUnlockEnrolled(this)) {
             mWorkHandler.post(() -> mFaceAuth.init());